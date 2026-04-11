ebcbf8488a24fb7be280028be20bc72507e0f732
Ankit <50232341+imankit007@users.noreply.github.com>
2026-04-11 23:52:55 +0530

    Implement MQTT packet structures and packing functions

--- src/mqtt.h
+++ src/mqtt.h
@@ -81,4 +81,129 @@ union mqtt_header {
   } bits;
 };
 
-#endif MQTT_H
+struct mqtt_connect {
+  union mqtt_header header;
+
+  union {
+    unsigned char byte;
+    struct {
+      int reserved : 1;
+      unsigned clean_session : 1;
+      unsigned will : 1;
+      unsigned will_qos : 2;
+      unsigned will_retain : 1;
+      unsigned password : 1;
+      unsigned username : 1;
+    } bits;
+  };
+
+  struct {
+    unsigned short keepalive;
+    unsigned char *client_id;
+    unsigned char *username;
+    unsigned char *password;
+    unsigned char *will_topic;
+    unsigned char *will_message;
+  } payload;
+};
+
+struct mqtt_connack {
+  union mqtt_header header;
+
+  union {
+    unsigned char byte;
+    struct {
+      unsigned session_present : 1;
+      unsigned reserved : 7;
+    } bits;
+  };
+  unsigned char rc;
+};
+
+struct mqtt_subscribe {
+  union mqtt_header header;
+  unsigned short pkt_id;
+  unsigned short tuples_len;
+  struct {
+    unsigned short topic_len;
+    unsigned char *topic;
+    unsigned qos;
+  } *tuples;
+};
+
+struct mqtt_unsubscribe {
+  union mqtt_header header;
+  unsigned short pkt_id;
+  unsigned short tuples_len;
+  struct {
+    unsigned short topic_len;
+    unsigned char *topic;
+  } *tuples;
+};
+
+struct mqtt_suback {
+  union mqtt_header header;
+  unsigned short pkt_id;
+  unsigned short rcslen;
+  unsigned char *rcs;
+};
+
+struct mqtt_publish {
+  union mqtt_header header;
+  unsigned short pkt_id;
+  unsigned short topiclen;
+  unsigned char *topic;
+  unsigned short payloadlen;
+  unsigned char *payload;
+};
+
+struct mqtt_ack {
+  union mqtt_header header;
+  unsigned short pkt_id;
+};
+
+typedef struct mqtt_ack mqtt_puback;
+typedef struct mqtt_ack mqtt_pubrec;
+typedef struct mqtt_ack mqtt_pubrel;
+typedef struct mqtt_ack mqtt_pubcomp;
+typedef struct mqtt_ack mqtt_unsuback;
+typedef union mqtt_header mqtt_pingreq;
+typedef union mqtt_header mqtt_pingres;
+typedef union mqtt_header mqtt_disconnect;
+
+union mqtt_packet {
+
+  struct mqtt_ack ack;
+  union mqtt_header header;
+
+  struct mqtt_connect connect;
+  struct mqtt_connack connack;
+
+  struct mqtt_suback suback;
+  struct mqtt_publish publish;
+
+  struct mqtt_subscribe subscribe;
+  struct mqtt_unsubscribe unsubscribe;
+};
+
+int mqtt_encode_length(unsigned char *, size_t);
+
+unsigned long long mqtt_decode_length(const unsigned char **);
+
+int unpack_mqtt_packet(const unsigned char *, union mqtt_packet *);
+
+unsigned char *pack_mqtt_packet(const union mqtt_packet *, unsigned);
+
+union mqtt_header *mqtt_packet_header(unsigned char);
+
+struct mqtt_ack *mqtt_packet_ack(unsigned char, unsigned short);
+
+struct mqtt_conack *mqtt_packet_connack(unsigned char, unsigned char, unsigned char);
+
+struct mqtt_suback *mqtt_packet_suback(unsigned char, unsigned short, unsigned char *, unsigned short);
+
+struct mqtt_publish *mqtt_packet_pubish(unsigned char, unsigned short, size_t, unsigned char *, size_t, unsigned char *);
+
+void mqtt_packet_release(union mqtt_packet *, unsigned);
+
+#endif
---
+++ src/pack.c
@@ -0,0 +1,5 @@
+#include <string.h>
+#include <stdlib.h>
+#include <winsock2.h>
+
+#include "pack.h"
